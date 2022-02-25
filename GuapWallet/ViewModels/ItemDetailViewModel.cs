9660c851433aef32a89c781d5b251026a021af0d
Ingrid Fields <ijfields@striketherockentertainment.com>
2022-02-24 19:25:14 -0800

    Add project files.

---
+++ GuapWallet/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GuapWallet.Models;
+
+namespace GuapWallet.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
